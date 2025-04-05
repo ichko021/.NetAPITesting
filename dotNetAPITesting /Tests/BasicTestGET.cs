5d447d39e19344ce8bdbf62f92d433cee989ba08
ichko021 <ichko021@abv.bg>
2025-04-05 12:16:04 +0300

    initial commit

---
+++ dotNetAPITesting /Tests/BasicTestGET.cs
@@ -0,0 +1,50 @@
+﻿using System.Net;
+using dotNetAPITesting.POCOs;
+using Newtonsoft.Json;
+
+namespace dotNetAPITesting.Tests;
+
+public class BasicTestGet
+{
+
+    private String url = "https://gorest.co.in/public/v2/users";
+    private HttpClient _httpClient;
+    
+    [SetUp]
+    public void Setup()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        _httpClient.Dispose();
+    }
+
+    [Test]
+    public async Task TestGet()
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync(url);
+
+        Console.Out.WriteLine(response);
+        
+        Assert.That(response.StatusCode.Equals(HttpStatusCode.OK),"Expected status code is 200 OK");
+        Assert.That(response.Content, Is.Not.Null, "Response content is not null");
+    }
+    
+    [Test]
+    public async Task TestGetWithResponse()
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync(url);
+        
+        Assert.That(response.IsSuccessStatusCode, Is.True, "Expected status code is 200 OK");
+        Assert.That(response.Content, Is.Not.Null, "Response content is not null");
+        
+        string responseData = await response.Content.ReadAsStringAsync();
+        List<UserResponse> users = JsonConvert.DeserializeObject<List<UserResponse>>(responseData);
+
+        Assert.That(users.Count, Is.Not.Zero, "Expected number of users returned is not 0");
+        Assert.That(users[0].name.Equals("Sen. Shantanu Kaniyar"), "First user's name is correct");
+    }
+}
