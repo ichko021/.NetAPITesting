e1ae8d328805bff57579a669361989c793517dc1
ichko021 <ichko021@abv.bg>
2025-04-05 12:19:12 +0300

    Adding pocos

---
+++ dotNetAPITesting /POCOs/UserResponse.cs
@@ -0,0 +1,10 @@
+namespace dotNetAPITesting.POCOs;
+
+public class UserResponse
+{
+    public int Id { get; set; }
+    public string name { get; set; }
+    public string email { get; set; }
+    public string gender { get; set; }
+    public string status { get; set; }
+}
